c106f2412acf85687a172d476a5ca94a1acaef31
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-12-26 18:14:48 +0530

    Add logs

--- src/main/scala/utils/UnifillUtils.scala
+++ src/main/scala/utils/UnifillUtils.scala
@@ -3,7 +3,7 @@ package utils
 import entity.UniwareShippingPackage
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Dataset, SparkSession}
-import utils.UniwareUtils.getJdbcUrlFromServerName
+import utils.UniwareUtils.{getJdbcUrlFromServerName, log}
 
 object UnifillUtils {
 
@@ -16,6 +16,7 @@ object UnifillUtils {
      *      https://stackoverflow.com/questions/71031178/how-to-make-spark-commit-in-each-batch-when-using-batchsize-and-writing-into-rdb
      */
     def writeUnifillJDBC(sparkSession: SparkSession, uniwareShippingPackageAddressDataset: Dataset[UniwareShippingPackage]): Unit = {
+        val start = System.currentTimeMillis()
         val sparkConf: SparkConf = sparkSession.sparkContext.getConf
         val jdbcOptions = Map(
             "url" -> sparkConf.get("spark.unifill.mysqldb.url"),
@@ -33,6 +34,9 @@ object UnifillUtils {
                 .options(jdbcOptions)
                 .mode("append")
                 .save()
+
+        log.info("Completed writeUnifillJDBC, count: " + uniwareShippingPackageAddressDataset.count +
+                " in " + ((System.currentTimeMillis() - start)/1000) + " sec")
     }
 
 }
